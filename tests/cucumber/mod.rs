5ebb75bc76c317dd29f90756b91ae37737c2ed0b
Fin Christensen <fin.christensen@posteo.de>
2022-04-16 14:01:05 +0200

    Fix gpg key generation in tests

--- tests/cucumber/mod.rs
+++ tests/cucumber/mod.rs
@@ -39,6 +39,27 @@ async fn main() {
     }
 }
 
+fn create_dsa_elgamal_keypair(ctx: &mut Context, name: &str) -> anyhow::Result<()> {
+    let result = ctx.create_key_with_flags(
+        name,
+        "DSA",
+        Duration::from_secs(0),
+        CreateKeyFlags::SIGN | CreateKeyFlags::NOEXPIRE,
+    ).context("Failed to create signing GPG key")?;
+    let key = ctx.get_key(
+        result.fingerprint().ok()
+            .context("fingerprint not utf-8")?
+    ).context("Could not find key")?;
+    ctx.create_subkey_with_flags(
+        &key,
+        "ELG",
+        Duration::from_secs(0),
+        CreateKeyFlags::ENCR | CreateKeyFlags::NOEXPIRE,
+    ).context("Failed to create encryption GPG key")?;
+
+    Ok(())
+}
+
 fn initialize_pgp_home(home: &Path) -> anyhow::Result<()> {
     env::remove_var("HOME");
     env::set_var("GNUPGHOME", home.join(".gnupg"));
@@ -86,33 +107,22 @@ fn initialize_pgp_home(home: &Path) -> anyhow::Result<()> {
     ctx.set_pinentry_mode(PinentryMode::Loopback)
         .context("Could not set pinentry mode in GPG")?;
 
-    ctx.create_key_with_flags(
-        "Test Key <test@key.email>",
-        "default",
-        Duration::from_secs(0),
-        CreateKeyFlags::SIGN | CreateKeyFlags::ENCR | CreateKeyFlags::NOEXPIRE,
-    ).context("Failed to create GPG key")?;
-    ctx.create_key_with_flags(
-        "Test Key 2 <test2@key.email>",
-        "default",
-        Duration::from_secs(0),
-        CreateKeyFlags::SIGN | CreateKeyFlags::ENCR | CreateKeyFlags::NOEXPIRE,
-    ).context("Failed to create GPG key")?;
-    ctx.create_key_with_flags(
-        "Test Key 3 <test3@key.email>",
-        "default",
-        Duration::from_secs(0),
-        CreateKeyFlags::SIGN | CreateKeyFlags::ENCR | CreateKeyFlags::NOEXPIRE,
-    ).context("Failed to create GPG key")?;
+    create_dsa_elgamal_keypair(&mut ctx, "Test Key <test@key.email>")
+        .context("Failed to create test@key.email keypairs")?;
+    create_dsa_elgamal_keypair(&mut ctx, "Test 2 Key <test2@key.email>")
+        .context("Failed to create test2@key.email keypairs")?;
+    create_dsa_elgamal_keypair(&mut ctx, "Test 3 Key <test3@key.email>")
+        .context("Failed to create test3@key.email keypairs")?;
     let key = ctx.get_secret_key("test@key.email")?;
-    let keygrip = key.subkeys().next().unwrap().keygrip().unwrap();
 
-    Command::new("/usr/libexec/gpg-preset-passphrase")
-        .args(&["--preset", "--passphrase", "test1234", keygrip])
-        .env_clear()
-        .env("GNUPGHOME", home.join(".gnupg"))
-        .output()
-        .expect("Could not set passphrase in gpg-agent");
+    for subkey in key.subkeys() {
+        Command::new("/usr/libexec/gpg-preset-passphrase")
+            .args(&["--preset", "--passphrase", "test1234", subkey.keygrip().unwrap()])
+            .env_clear()
+            .env("GNUPGHOME", home.join(".gnupg"))
+            .output()
+            .expect("Could not set passphrase in gpg-agent");
+    }
 
     println!("GPG environment initialized for all tests");
 
